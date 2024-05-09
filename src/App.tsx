ae59fbdfda0e340836df81b9cf6c390f824f5b76
Cody Lee <notlimaheelydoc@gmail.com>
2024-05-09 15:20:19 -0500

    making component lib

--- src/App.tsx
+++ src/App.tsx
@@ -1,68 +1,29 @@
 import "./styles.css";
-import { LightBorder } from "./components/LightBorder";
-import Button from "./components/button";
+import { LightBorder,LightButton } from "./components/LightBorder";
+
 
 export default function App(): JSX.Element {
   return (
     <div className="App">
       <LightBorder
-        $color1="red"
-        $color2="red"
-        $color3="black"
-        $borderWidth={20}
-        $borderRadius={10}
-        $speed="1s"
-        $contentColor="grey"
-        $height="100%"
-        $width="100%"
+      $borderWidth={20}
+      $contentAlignY="center"
+      $contentAlignX="center"
+      $color1="yellow"
+      $color2="lime"
+      $color3="cyan"
       >
-      
-
-
-<LightBorder>  
-  <LightBorder
-        $color1="pink"
-        $color2="pink"
-        $color3="maroon"
-        $borderWidth={55}
-        $borderRadius={1}
-        $speed="4s"
-        $contentColor="white"
-        $height="50%"
-        $width="50%"
-      >
-        hi
-      </LightBorder></LightBorder>
-
+     
+     <LightButton
+     $borderWidth={4}
+     $borderRadius={100}
+     >tester</LightButton>
 
 
       </LightBorder>
-      {/* <Button /> */}
+      </div>
     
-    </div>
+    
   );
 }
 
-/*
-
-Here, we can see that we're getting this html rendered:
-Looking at the styles... I am inclined to look at the keyframes css.
-<div class="App">
-  <div class="sc-beqWNU hjNioH">
-    <div class="sc-gueXAH ekXUHE"></div>
-    <div class="sc-dmqGRC kJBPwM"></div>
-    <div class="sc-hLseSp hlLgjJ">hi</div>
-    <div class="sc-eDDOjk foAWsq"></div>
-    <div class="sc-jTrOVX fYkrvJ"></div>
-  </div>
-  <div class="sc-beqWNU ffVLRT">
-    <div class="sc-gueXAH fiyWuL"></div>
-    <div class="sc-dmqGRC igvftD"></div>
-    <div class="sc-hLseSp cXiUor">hi</div>
-    <div class="sc-eDDOjk dlWtXB"></div>
-    <div class="sc-jTrOVX eJjPMi"></div>
-  </div>
-</div>
-
-
-*/
--- src/components/LightBorder.tsx
+++ src/components/LightBorder.tsx
@@ -11,13 +11,62 @@ interface LightProps {
   $color1?: string;
   $color2?: string;
   $color3?: string;
+  $contentAlignX?:string;
+  $contentAlignY?:string;
 }
 
-const Container = styled.div<LightProps>`
+
+const Content = styled.div<LightProps>`
+  position: absolute;
+ padding:0px;
+  top: ${(props) => props.$borderWidth ?? 8}px;
+  bottom: ${(props) => props.$borderWidth ?? 8}px;
+  left: ${(props) => props.$borderWidth ?? 8}px;
+  right: ${(props) => props.$borderWidth ?? 8}px;
+  z-index: 1;
+
+  border: ${(props) => props.$contentColor ?? "black"} 1px solid;
+  border-radius: ${(props) =>
+    props.$borderRadius ? 5 : 0}px;
+  background-color: ${(props) => props.$contentColor ?? "black"};
+`;
+const DivContainer = styled.div<LightProps>`
+  
   position: relative;
+  
   height: ${(props) => props.$height ?? "100%"};
   width: ${(props) => props.$width ?? "100%"};
 `;
+
+const DivCenter=styled.div<LightProps>`
+height:100%;
+width:100%;
+margin:auto;
+justify-content: ${(props)=>props.$contentAlignX ?? "none"};
+align-items:${(props)=>props.$contentAlignY ?? "none"} ;
+display: flex;
+`
+const ButtonContainer = styled.div<LightProps>`
+  
+  position: relative;
+  :hover {
+    cursor:pointer;
+  }
+  :active {
+    transform:scale(.95);
+  }
+
+`;
+const ButtonCenter=styled.div<LightProps>`
+padding:10px;
+height:100%;
+width:100%;
+justify-content:center;
+align-items:center;
+display:flex;
+position:relative;
+
+`
 const Top = styled.div<LightProps>`
   position: absolute;
   top: 0;
@@ -170,18 +219,7 @@ const Left = styled.div<LightProps>`
     }
   }
 `;
-const Content = styled.div<LightProps>`
-  position: absolute;
-  top: ${(props) => props.$borderWidth ?? 10}px;
-  bottom: ${(props) => props.$borderWidth ?? 10}px;
-  left: ${(props) => props.$borderWidth ?? 10}px;
-  right: ${(props) => props.$borderWidth ?? 10}px;
-  z-index: 1;
-  border: ${(props) => props.$contentColor ?? "black"} 1px solid;
-  border-radius: ${(props) =>
-    props.$borderRadius ? props.$borderRadius / 2 : 0}px;
-  background-color: ${(props) => props.$contentColor ?? "black"};
-`;
+
 const Right = styled.div<LightProps>`
   position: absolute;
   top: 0;
@@ -345,12 +383,38 @@ export const LightBorder: React.FC<{ children: ReactNode } & LightProps> = ({
   } as React.CSSProperties
 
   return (
-    <Container {...props} style={cssVariables}>
+    <DivContainer {...props} style={cssVariables}>
       <Top {...props} />
       <Left {...props} />
-      <Content {...props}>{children}</Content>
+      <Content {...props}><DivCenter {...props}>{children}</DivCenter></Content>
       <Right {...props} />
       <Bottom {...props} />
-    </Container>
+    </DivContainer>
+  );
+};
+export const LightButton: React.FC<{ children: ReactNode } & LightProps> = ({
+ children,
+  ...props
+}) => {
+  const cssVariables = {
+    '--color1': props.$color1 ?? "hotpink",
+    '--color2': props.$color2 ?? "cyan",
+    '--color3': props.$color3 ?? "lime"
+  } as React.CSSProperties
+
+  return (
+    
+    <ButtonContainer {...props} style={cssVariables}>
+     <ButtonCenter>
+      <Top {...props} />
+      <Left {...props} />
+      
+      {children}
+      
+      <Right {...props} />
+      <Bottom {...props} />
+      </ButtonCenter>
+    </ButtonContainer>
+   
   );
 };
