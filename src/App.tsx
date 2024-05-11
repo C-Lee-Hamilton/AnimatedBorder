997a5182f9c973fc5b5abdc7e068265319c9af2f
Cody Lee <notlimaheelydoc@gmail.com>
2024-05-10 23:05:16 -0500

    added sliderbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,49 @@
 import "./styles.css";
-import { LightBorder,LightButton } from "./components/LightBorder";
+import React,{useState} from 'react';
+import { LightBorder,LightButton,LightSlider } from "./components/LightBorder";
 
 
 export default function App(): JSX.Element {
+  const [sliderValue,setSliderValue]=useState<number>(5);
+  const handleSlideChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newSliderValue = parseInt(event.target.value, 10);
+    setSliderValue(newSliderValue);
+  };
   return (
     <div className="App">
       <LightBorder
       $borderWidth={20}
       $contentAlignY="center"
       $contentAlignX="center"
-      $color1="black"
-      $color2="black"
-      $color3="blue"
+      $color1="lime"
+      $color2="lightgreen"
+      $color3="white"
       $rounded={true}
       >
-     
+     <div className="showcaseCol">
      <LightButton
-     $fontsize=""
+ $color1="cyan"
+ $color2="magenta"
+ $color3="cyan"
+
+    $fontsize=""
     $borderWidth={5}
-     $rounded={true}
+    $rounded={true}
     $onClick={()=>console.log("it worked again")}
      >tester</LightButton>
+      
+    <LightSlider
+    $roundedSelector={true}
+    value={sliderValue}
+    onChange={handleSlideChange}
+    step={1}
+    min={0}
+    max={10}
+    />
+   
+    </div>
 
-
+ 
       </LightBorder>
       </div>
     
--- src/components/LightBorder.tsx
+++ src/components/LightBorder.tsx
@@ -20,11 +20,16 @@ interface DivProps {
 interface ButtonProps{
 $fontsize?:string;
 $onClick?: () => void;
+$margin?:string;
 
 }
 interface SliderProps{
-  
-  $value?:number,
+  min?:number,
+  max?:number,
+  step?:number,
+  value?:number,
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void; // Update the type here
+  $roundedSelector?:boolean,
 }
 const DivContainer = styled.div<LightProps>`
   
@@ -60,7 +65,7 @@ const ButtonContainer = styled.div<LightProps & ButtonProps >`
 height: ${(props) => props.$height ?? "30px"};
 width: ${(props) => props.$width ?? "100px"};
 font-size:${(props)=>props.$fontsize ?? "1em"};
-
+margin:${(props)=>props.$margin ?? "auto"};
   position: relative;
   :hover {
     cursor:pointer;
@@ -101,6 +106,148 @@ z-index:1;
   props.$rounded ? 5 : 0}px;
 
 background-color: ${(props) => props.$contentColor ?? "black"};
+
+
+`
+const SliderContainer=styled.div<LightProps & SliderProps>`
+
+width: ${(props)=>props.$width ?? "300px"};
+height: ${(props)=>props.$height ?? "20px"};
+padding:2px;
+
+border-radius: ${(props)=>props.$roundedSelector ? "100px" : "0px"};
+
+animation: ColorSlide ${(props) => props.$speed ?? "1s"} infinite linear;
+  @keyframes ColorSlide {
+    
+    0% {
+      background-image: linear-gradient(
+        to right,
+        var(--color1),
+        var(--color2),
+        var(--color2),
+        var(--color3),
+        var(--color3),
+        var(--color1)
+      );
+    }
+    20% {
+      background-image: linear-gradient(
+        to right,
+        var(--color1),
+        var(--color1),
+        var(--color2),
+        var(--color2),
+        var(--color3),
+        var(--color3)
+      );
+    }
+    40% {
+      background-image: linear-gradient(
+        to right,
+        var(--color3),
+        var(--color1),
+        var(--color1),
+        var(--color2),
+        var(--color2),
+        var(--color3)
+      );
+    }
+    60% {
+      background-image: linear-gradient(
+        to right,
+        var(--color3),
+        var(--color3),
+        var(--color1),
+        var(--color1),
+        var(--color2),
+        var(--color2)
+      );
+    }
+    80% {
+      background-image: linear-gradient(
+        to right,
+        var(--color2),
+        var(--color3),
+        var(--color3),
+        var(--color1),
+        var(--color1),
+        var(--color2)
+      );
+    }
+    100% {
+      background-image: linear-gradient(
+        to right,
+        var(--color2),
+        var(--color2),
+        var(--color3),
+        var(--color3),
+        var(--color1),
+        var(--color1)
+      );
+    }
+`
+const Slider=styled.input<LightProps & SliderProps>`
+-webkit-appearance:none;
+appearance: none;
+margin:1px;
+border-radius: ${(props)=>props.$roundedSelector ? "100px" : "0px"};
+
+width: 99%;
+height: 85%;
+background: black;
+outline: none;
+opacity: 1;
+-webkit-transition: 0.2s;
+transition: opacity 0.2s;
+cursor: pointer;
+
+ @keyframes ColorSlideSelect {
+    
+  0% {
+    box-shadow: 0px 0px 0px 4px var(--color2), 0px 0px 0px 5px var(--color3) inset, 0px 0px 0px 10px black inset;
+    background: var(--color1);
+
+  }
+  50% {
+    box-shadow: 0px 0px 0px 4px var(--color3), 0px 0px 0px 5px var(--color1) inset, 0px 0px 0px 10px black inset;
+    background: var(--color2);
+  }
+
+  
+  100% {
+    box-shadow: 0px 0px 0px 4px var(--color1), 0px 0px 0px 5px var(--color2) inset, 0px 0px 0px 10px black inset;
+    background: var(--color3);
+  }}
+
+&::-webkit-slider-thumb {
+ 
+  -webkit-appearance: none;
+  background: var(--color1);
+
+  background-origin: border-box;
+  background-clip: content-box, border-box;
+  height:2.5em;
+  width:2.5em;
+  border: 4px black solid;
+  border-radius: ${(props)=>props.$roundedSelector ? "100px" : "0px"};
+  box-shadow: 0px 0px 0px 4px var(--color2), 0px 0px 0px 5px var(--color3) inset, 0px 0px 0px 10px black inset;
+
+  animation: ColorSlideSelect 5s infinite linear;
+
+}
+
+&::-moz-range-thumb {
+  background-origin: border-box;
+  background-clip: content-box, border-box;
+  height:2em;
+  width:2em;
+  border: 4px black solid;
+  border-radius: ${(props)=>props.$roundedSelector ? "100px" : "0px"};
+  background: var(--color1);
+  
+        animation: ColorSlideSelect 5s infinite reverse;
+}
 
 
 `
@@ -464,3 +611,22 @@ export const LightButton: React.FC<{ children: ReactNode } & LightProps & Button
    
   );
 };
+export const LightSlider: React.FC< LightProps & SliderProps > = ({
+  
+   ...props
+ }) => {
+   const cssVariables = {
+     '--color1': props.$color1 ?? "hotpink",
+     '--color2': props.$color2 ?? "cyan",
+     '--color3': props.$color3 ?? "lime"
+   } as React.CSSProperties
+
+   return (
+    <SliderContainer  style={cssVariables} {...props}>
+      <Slider type="range" onChange={props.onChange}value={props.value}step={props.step} min={props.min} max={props.max} {...props}></Slider>
+ 
+   
+   </SliderContainer>
+    
+   );
+ };
